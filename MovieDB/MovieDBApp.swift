85ce816146059c8ba88ca1c58fc25efa8be05e3a
Umair Irfan <umair.irfan@starzplayarabia.com>
2023-08-08 00:42:14 +0500

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by umair irfan on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
